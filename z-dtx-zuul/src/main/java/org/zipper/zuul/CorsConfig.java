58bca1dc1d49ed8e5717091c55ae0c6e2a429a2f
zhuxj <870324066@qq.com>
2020-11-20 15:09:26 +0800

    解决zuul网关跨域请求

---
+++ z-dtx-zuul/src/main/java/org/zipper/zuul/CorsConfig.java
@@ -0,0 +1,28 @@
+package org.zipper.zuul;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    public CorsConfig() {
+    }
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", this.buildConfig());
+        return new CorsFilter(source);
+    }
+}
